b35ca2f9d775420004aa13e4fb60342f1de497c2
Veerakiat P <gunnermontana@hotmail.com>
2021-10-31 14:22:43 +0700

    Create Product Repository

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContextcs.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Driver;
+using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextcs : ICatalogContext
+    {
+        public IMongoCollection<Product> Products { get; }
+
+        public CatalogContextcs(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+        }
+    }
+}
