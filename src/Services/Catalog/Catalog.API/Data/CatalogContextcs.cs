502e8085422e92d165e9e1776e067eb47a891cab
Veerakiat P <gunnermontana@hotmail.com>
2021-10-31 16:52:48 +0700

    Create Catalog Controller

--- src/Services/Catalog/Catalog.API/Data/CatalogContextcs.cs
+++ src/Services/Catalog/Catalog.API/Data/CatalogContextcs.cs
@@ -4,11 +4,11 @@ using Microsoft.Extensions.Configuration;
 
 namespace Catalog.API.Data
 {
-    public class CatalogContextcs : ICatalogContext
+    public class CatalogContext : ICatalogContext
     {
         public IMongoCollection<Product> Products { get; }
 
-        public CatalogContextcs(IConfiguration configuration)
+        public CatalogContext(IConfiguration configuration)
         {
             var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
             var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
