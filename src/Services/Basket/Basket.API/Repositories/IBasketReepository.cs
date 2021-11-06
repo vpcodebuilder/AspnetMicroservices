b67affb0dc099b4e13e81a5882a5441d66a60183
Veerakiat P <gunnermontana@hotmail.com>
2021-11-06 23:31:45 +0700

    Create Basket.API

---
+++ src/Services/Basket/Basket.API/Repositories/IBasketReepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Basket.API.Entities;
+
+namespace Basket.API.Repositories
+{
+    public interface IBasketReepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+
+        Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+
+        Task DeleteBasket(string userName);
+    }
+}
